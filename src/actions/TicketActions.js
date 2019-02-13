6dad5c20bf1010a8d9f80e5605b9d2f265160e04
Naveen Anem <KH2475@kitspl.com>
2019-02-13 15:59:31 +0530

    Updating PUT with multipart form data

--- src/actions/TicketActions.js
+++ src/actions/TicketActions.js
@@ -64,6 +64,7 @@ export function createTicketAPICall(ticket){
     for(var name in ticket) {
         formData.append(name, ticket[name]);
       }
+      console.log(JSON.stringify(formData));
     return function (dispatch) {      
         return fetch(`http://localhost:8080/v0/ticket-management/tickets`,{
             method : 'POST',
@@ -167,9 +168,36 @@ export function addMessageAPICall(params){
 }
 
 export function closeTicketAPICall(params){
-    console.log(params);
     let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
+    /* headers.append('Content-Type', 'multipart/form-data'); */
+    headers.append('Authorization', 'Bearer ' + localStorage.getItem('token'));
+    var formData = new FormData();
+    for(var name in params) {
+        formData.append(name, params[name]);
+      }
+    return function (dispatch) {      
+        return fetch(`http://localhost:8080/v0/ticket-management/tickets/3`,{
+            method : 'PUT',
+            body : formData,
+            headers: headers
+        })
+        .then(
+           response => {
+               console.log(response);
+               if(response.status === 201){
+                dispatch(createTicketSuccess());
+               return response.statusText;
+               }
+           },
+           error => {
+            console.log('An error occurred.', error);
+           }
+       );
+      };
+}
+/* export function closeTicketAPICall(params){
+    console.log(params);
+    let headers = new Headers();    
     headers.append('Authorization', 'Bearer ' + localStorage.getItem('token'));
     var formData = new FormData();
     for(var name in params) {
@@ -196,5 +224,5 @@ export function closeTicketAPICall(params){
            }
        );
       };
-}
+} */
 
--- src/containers/ViewTicketDetailsForm.jsx
+++ src/containers/ViewTicketDetailsForm.jsx
@@ -12,9 +12,7 @@ class ViewTicketDetailsForm extends React.Component {
       id:this.props.ticket.id,
       status: '',
       comment:'',
-      file1:'',
-      file2:'',
-      file3:'',
+      
       commentedOn:''
     };
 
