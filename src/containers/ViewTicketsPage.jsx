e1a9286799e331cf65f7145025f9b5b9b3574b8d
Naveen Anem <KH2475@kitspl.com>
2019-03-21 13:38:17 +0530

    Removing unwanted code

--- src/containers/ViewTicketsPage.jsx
+++ src/containers/ViewTicketsPage.jsx
@@ -17,13 +17,7 @@ class ViewTicketsPage extends React.Component {
   }
 
   componentDidMount() {
-    const queryParams = new URLSearchParams(this.props.location.search);
-    const status = queryParams.get('status');
-    /* this.props.fetchTickets({
-      userId:"naveen.anem@kony.com",
-      status: status,          
-      sortBy:'ticketId'
-    }); */
+    
   }
 
   loadTickets() {
