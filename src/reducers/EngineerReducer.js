09a4ef2c83e97d994fbfa2277c535d115f553633
Naveen Anem <KH2475@kitspl.com>
2019-03-25 15:04:33 +0530

    Removing unwanted code

--- src/reducers/EngineerReducer.js
+++ src/reducers/EngineerReducer.js
@@ -1,16 +1,7 @@
 import {LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT} from '../actions/ActionTypes';
 
 export function engineerReducer(state = {}, action){
-    switch(action.type){
-        case LOGIN_SUCCESS: 
-        return { ...state, isLoggedIn: true};
-        
-        case LOGIN_FAILURE:
-        return {...state, isLoginFailure: true, loginFailureMessage: 'Invalid Credentials. Please try again.', isLoggedIn: false}
-
-        case LOGOUT:
-        return {...state, isLoggedIn: false}
-        
+    switch(action.type){                
         default:
         return state;
     }
