56899bda0b53eff5f8080d58d3df115c94ba625c
Naveen Anem <KH2475@kitspl.com>
2019-04-03 16:25:50 +0530

    Fixing SideNavbar focus

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -11,6 +11,16 @@ class SideNavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dashboardStyle : { borderTopLeftRadius:'10px',borderTopRightRadius:'10px',borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' },
+      assignticketsStyle : { borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' },
+      awaitresponseStyle : { borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' },
+      newticketStyle : { borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' },
+      myticketsStyle : { borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' },
+      closedticketsStyle : { borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' },
+      disabledLink : { borderBottomLeftRadius:'10px', borderBottomRightRadius:'10px', textDecoration: 'none', color: '#546e7a' }
+      
+
+
     };
     this.handleClosedTicketsClick = this.handleClosedTicketsClick.bind(this);
     this.handleAssignTicketsClick = this.handleAssignTicketsClick.bind(this);
@@ -18,10 +28,11 @@ class SideNavBar extends React.Component {
     this.handleAwaitResponseClick = this.handleAwaitResponseClick.bind(this);
     this.handleDashboard = this.handleDashboard.bind(this);
     this.handleNewTicket = this.handleNewTicket.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
   }
 
   handleDashboard(e) {
-    history.push("/dashboard");
+    history.push({pathname: "/dashboard", key:'dashboard'});
     this.props.fetchDashboardData();
   }
 
@@ -35,6 +46,7 @@ class SideNavBar extends React.Component {
 
   handleClosedTicketsClick() {
     history.push("/tickets?status=Closed");
+    this.handleFocus();
     this.props.fetchTickets({
       status: 'Closed',
       sortBy: 'ticketId'
@@ -43,6 +55,7 @@ class SideNavBar extends React.Component {
 
   handleAssignTicketsClick() {
     history.push("/tickets?status=New");
+    this.handleFocus();
     this.props.fetchTickets({
       status: 'New',
       sortBy: 'ticketId'
@@ -52,6 +65,7 @@ class SideNavBar extends React.Component {
 
   handleMyTicketsClick() {
     history.push("/tickets?status=all");
+    this.handleFocus();
     this.props.fetchTickets({
       status: 'all',
       sortBy: 'ticketId'
@@ -61,7 +75,78 @@ class SideNavBar extends React.Component {
 
   handleNewTicket() {
     history.push("/newticket");
+    this.handleFocus();
     this.props.showNewTicketForm();
+  }
+
+  componentDidMount(){
+    this.handleFocus();
+  }
+
+  handleFocus(){
+    if(history.location.pathname == '/dashboard'){
+      const focusStyle = { backgroundColor:'#d4d8d8', borderTopLeftRadius:'10px',borderTopRightRadius:'10px',borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' }
+      console.log("Setting focus style to Dashboard option");
+      this.setState({
+        dashboardStyle : focusStyle
+      });
+    }
+
+    if(history.location.pathname == '/tickets' && history.location.search == '?status=New'){
+      const focusStyle = { backgroundColor:'#d4d8d8', borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' };
+      const blurStyle = { borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' };
+      console.log("Setting focus style to AssignTickets option");
+      this.setState({
+        closedticketsStyle : blurStyle,
+        myticketsStyle : blurStyle,
+        awaitresponseStyle: blurStyle,
+        assignticketsStyle: focusStyle
+      });
+    }
+
+    if(history.location.pathname == '/tickets' && history.location.search == '?status=InProcess'){
+      const focusStyle = { backgroundColor:'#d4d8d8', borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' };
+      const blurStyle = {  borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' };
+      console.log("Setting focus style to AwaitResponse option");
+      this.setState({
+        closedticketsStyle : blurStyle,
+        myticketsStyle : blurStyle,
+        awaitresponseStyle: focusStyle,
+        assignticketsStyle: blurStyle
+      });
+    }
+
+    if(history.location.pathname == '/newticket'){
+      const focusStyle = { backgroundColor:'#d4d8d8', borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' }
+      console.log("Setting focus style to NewTicket option");
+      this.setState({
+        newticketStyle : focusStyle
+      });
+    }
+
+    if(history.location.pathname == '/tickets' && history.location.search == '?status=all'){
+      const focusStyle = { backgroundColor:'#d4d8d8', borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' };
+      const blurStyle = {  borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' };
+      console.log("Setting focus style to MyTickets option");
+      this.setState({
+        closedticketsStyle : blurStyle,
+        myticketsStyle : focusStyle,
+        awaitresponseStyle: blurStyle,
+        assignticketsStyle: blurStyle
+      });
+    }
+
+    if(history.location.pathname == '/tickets' && history.location.search == '?status=Closed'){
+      const focusStyle = { backgroundColor:'#d4d8d8', borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' };
+      const blurStyle = {  borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' };
+      console.log("Setting focus style to ClosedTickets option");
+      this.setState({
+        closedticketsStyle : focusStyle,
+        myticketsStyle : blurStyle,
+        awaitresponseStyle: blurStyle,
+        assignticketsStyle: blurStyle
+      });
+    }
   }
 
 
@@ -73,31 +158,31 @@ class SideNavBar extends React.Component {
 
         <Nav style={{ borderRadius: '10px', border: '1px solid grey', backgroundColor: '#E8EAED' }} vertical >
           <NavItem >
-            <NavLink href="#" onClick = {this.handleDashboard}style={{ borderTopLeftRadius:'10px',borderTopRightRadius:'10px',borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' }}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> Dashboard
+            <NavLink href="#" onClick = {this.handleDashboard} style={this.state.dashboardStyle}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> Dashboard
             </NavLink>
           </NavItem>
 
           <NavItem>
-            <NavLink href="#" onClick={this.handleAssignTicketsClick} style={{ borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' }}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> Assign Tickets
+            <NavLink href="#" onClick={this.handleAssignTicketsClick} style={this.state.assignticketsStyle}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> Assign Tickets
             </NavLink>
           </NavItem>
 
           <NavItem>
-            <NavLink href="#" onClick={this.handleAwaitResponseClick} style={{ borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' }}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> Await Response
+            <NavLink href="#" onClick={this.handleAwaitResponseClick} style={this.state.awaitresponseStyle}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> Await Response
             </NavLink>
           </NavItem>
 
           <NavItem>
-            <NavLink href="#" onClick={this.handleNewTicket} style={{ borderBottom: '1px solid #bdb7b7', marginBottom: '', textDecoration: 'none', color: '#546e7a' }}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> New Ticket
+            <NavLink href="#" onClick={this.handleNewTicket} style={this.state.newticketStyle}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> New Ticket
             </NavLink>
           </NavItem>
 
           <NavItem>
-            <NavLink href="#" onClick={this.handleMyTicketsClick} style={{ borderBottom: '1px solid #bdb7b7', textDecoration: 'none', color: '#546e7a' }}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> My Tickets</NavLink>
+            <NavLink href="#" onClick={this.handleMyTicketsClick} style={this.state.myticketsStyle}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> My Tickets</NavLink>
           </NavItem>
 
           <NavItem>
-            <NavLink href="#" onClick={this.handleClosedTicketsClick} style={{ borderBottom: '1px solid #bdb7b7', textDecoration: 'none', color: '#546e7a' }}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> Closed Tickets</NavLink>
+            <NavLink href="#" onClick={this.handleClosedTicketsClick} style={this.state.closedticketsStyle}><FaAngleDoubleRight style={{ color: '#546e7a' }} /> Closed Tickets</NavLink>
           </NavItem>
 
           <NavItem>
