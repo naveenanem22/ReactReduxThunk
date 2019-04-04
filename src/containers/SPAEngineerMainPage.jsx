18e04b7b1c9de646de4329d94e85d866fdfb4098
Naveen Anem <KH2475@kitspl.com>
2019-04-04 14:54:17 +0530

    Layout design SPAEngineerMainPage in progress

--- src/containers/SPAEngineerMainPage.jsx
+++ src/containers/SPAEngineerMainPage.jsx
@@ -20,19 +20,19 @@ class SPAEngineerMainPage extends React.Component {
 
   render() {
     return (
-      <div>        
-          <div class='sticky' style={{width:'100%'}}>
-            <HeaderNavBar></HeaderNavBar>
-          </div>        
-        <Row>
-          <Col><SideNavBar></SideNavBar></Col>
-          <Col>
-            <Route path="/ticketmaint/dashboard" component={DashboardForm}></Route>
-            <Route path="/ticketmaint/newticket" component={CreateTicketForm}></Route>
-          </Col>
-          <Col>
-            <div>This is where the sub content to be displayed..like Org uupdates, new tickets, closed ticets</div>
-          </Col>
+      <div >
+        <div class='sticky' style={{ height: '100%', width: '100%' }}>
+          <HeaderNavBar></HeaderNavBar>
+        </div>
+        <Row style={{ background: 'rgba(0,0,0,0.3)', marginTop:'1%', marginLeft:'5%', marginRight:'3%' }}>
+            <Col sm='2'><SideNavBar></SideNavBar></Col>
+            <Col sm='7'>
+              <Route path="/ticketmaint/dashboard" component={DashboardForm}></Route>
+              <Route path="/ticketmaint/newticket" component={CreateTicketForm}></Route>
+            </Col>
+            <Col sm='3'>
+              <div style={{ position: 'sticky', top: 100, zIndex: 1 }}>This is where the sub content to be displayed..like Org uupdates, new tickets, closed ticets</div>
+            </Col>
         </Row>
       </div>
     );
