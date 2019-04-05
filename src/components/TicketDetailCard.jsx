e1a13492e1a48b7fa565d7eabfab7173c9916d9c
Naveen Anem <KH2475@kitspl.com>
2019-04-05 11:46:44 +0530

    Improved design

--- src/components/TicketDetailCard.jsx
+++ src/components/TicketDetailCard.jsx
@@ -19,10 +19,10 @@ class TicketDetailCard extends React.Component{
 
     render(){
         return (
-            <Card style={{ width: '100%', borderLeft: '1%' }}>
+            <Card style={{  width: '100%', boxShadow:'0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }}>
                 <CardHeader style={{ paddingTop: '1%', paddingBottom: '1%', paddingRight:'0' }}><Row>
                     <Col sm='2' style={{ textAlign: 'left' }}><Badge color="danger">High</Badge></Col>
-                    <Col sm='4' style={{ textAlign: 'center' }}>112658</Col>
+                    <Col sm='4' style={{ textAlign: 'center' }}> Ticket # 112658</Col>
                     <Col sm='3' style={{ textAlign: 'right', color: '#0000008a', fontSize: '80%', margin: '0', paddingRight: '0' }}><i>Created by</i></Col>
                     <Col sm='3' style={{ textAlign: 'left', fontWeight: '500', color: '#0000008a', fontSize: '80%', margin: '0' }}>Naveen Kumar Anem</Col>
                 </Row></CardHeader>
@@ -30,13 +30,15 @@ class TicketDetailCard extends React.Component{
                     <Row>
                         <Col sm='8'>
                             <Row><Col style={{ fontSize: '90%', fontWeight: 600 }}>Special Title Treatment</Col></Row>
-                            <Row><Col style={{ fontSize: '90%', fontWeight: 300 }}>With supporting text below as a natural lead-in to additional content.</Col></Row>
+                            <Row><Col style={{ fontSize: '90%', fontWeight: 300 }}>With supporting text below as a natural lead-in to additional content. 
+                            Adding the href prop (without specifying a tag prop) will default the badge to a link.</Col></Row>
 
                         </Col>
                         <Col sm='4' style={{ borderLeft: '1px solid rgba(0,0,0,.125)' }}>
-                            <Row><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}><i>Status</i></Col><Col></Col></Row>
-                            <Row><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}><i>Days Open</i></Col></Row>
-                            <Row><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}><i>Department</i></Col></Row>
+                            <Row><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}><i>Status</i></Col><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}>: Department</Col></Row>
+                            <Row><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}><i>Days Open</i></Col><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}>: 28</Col></Row>
+                            <Row><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}><i>Department</i></Col><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}>: Department</Col></Row>
+                            <Row><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}><i>Updated On</i></Col><Col style={{ color: '#0000008a', fontSize: '80%', textAlign: 'left' }}>: 20th Jan</Col></Row>
                         </Col>
                     </Row>
 
