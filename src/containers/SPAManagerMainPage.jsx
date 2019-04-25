64e96568bd0f159f04eca0086e03cda5e6ca5336
Naveen Anem <KH2475@kitspl.com>
2019-04-25 18:29:04 +0530

    SPAManagerMainPage nearing completion

--- src/containers/SPAManagerMainPage.jsx
+++ src/containers/SPAManagerMainPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import HeaderNavBar from "../components/EngineerHeaderNavBar";
+import HeaderNavBar from "../components/ManagerHeaderNavBar";
 import { connect } from 'react-redux';
 import CreateTicketForm from './CreateTicketForm';
-import DashboardForm from './EngineerDashboard';
+import DashboardForm from './DashboardForm';
 import ViewTicketsForm from './ViewTicketBundlesForm';
 import { Route } from 'react-router-dom'
-import SideNavBar from '../components/EngineerSideNavBar';
+import SideNavBar from '../components/ManagerSideNavBar';
 import {Row, Col} from 'reactstrap';
 import ViewTicketBundleDetailsForm from './ViewTicketBundleDetailsForm';
 import EngineerProfileForm from './EngineerProfile';
@@ -22,7 +22,7 @@ class SPAEngineerMainPage extends React.Component {
 
   updateRouteAndShowTicketBundleDetails(ticketId){
     history.push({
-      pathname:'/ticketmaint/tickets',
+      pathname:'/ticketmanage/tickets',
       search:'?ticketId='+ticketId
     });
     console.log("ticketId: "+ticketId);
@@ -44,16 +44,16 @@ class SPAEngineerMainPage extends React.Component {
         <Row style={{ background: 'rgba(0,0,0,0.3)', marginTop:'1%', marginLeft:'3%', marginRight:'3%', paddingTop:'1%' }}>
             <Col sm='2' ><SideNavBar></SideNavBar></Col>
             <Col sm='7' style={{border: '1px solid #E8EAED', borderRadius: '10px',  paddingRight: '5px',  paddingLeft:'5px',  paddingTop:'5px',  paddingBottom:'5px',  backgroundColor: '#ffffff'}}>
-              <Route path="/ticketmaint/dashboard" component={DashboardForm}></Route>
-              <Route path="/ticketmaint/newticket" component={CreateTicketForm}></Route>
-              <Route path="/ticketmaint/tickets" component={() => <ViewTicketsForm handleTicketBundleClick={this.updateRouteAndShowTicketBundleDetails}/>}></Route>
+              <Route path="/ticketmanage/dashboard" component={DashboardForm}></Route>
+              <Route path="/ticketmanage/newticket" component={CreateTicketForm}></Route>
+              <Route path="/ticketmanage/tickets" component={() => <ViewTicketsForm handleTicketBundleClick={this.updateRouteAndShowTicketBundleDetails}/>}></Route>
             </Col>
             <Col sm='3'>            
               <div style={{ position: 'sticky', top: 100, zIndex: 1 }}>
-              <Route path="/ticketmaint/tickets" component={() =><ViewTicketBundleDetailsForm showSelectTicketMsg={this.state.showSelectTicketMsg}></ViewTicketBundleDetailsForm>}> </Route>
+              <Route path="/ticketmanage/tickets" component={() =><ViewTicketBundleDetailsForm showSelectTicketMsg={this.state.showSelectTicketMsg}></ViewTicketBundleDetailsForm>}> </Route>
               
               </div>
-              <Route path="/ticketmaint/dashboard" component={() =><EngineerProfileForm ></EngineerProfileForm>}> </Route>
+              <Route path="/ticketmanage/dashboard" component={() =><EngineerProfileForm ></EngineerProfileForm>}> </Route>
             </Col>
         </Row>
       </div>
