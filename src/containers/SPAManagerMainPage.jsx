7328f5e2cf69d1a5eead7e35506c0c97725c401b
Naveen Anem <KH2475@kitspl.com>
2019-04-26 23:47:02 +0530

    Formatted

--- src/containers/SPAManagerMainPage.jsx
+++ src/containers/SPAManagerMainPage.jsx
@@ -7,7 +7,7 @@ import ViewTicketsBundlesForm from './ViewTicketBundlesForm';
 import ViewTicketsForm from './ViewTicketsForm';
 import { Route } from 'react-router-dom'
 import SideNavBar from '../components/ManagerSideNavBar';
-import {Row, Col} from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import ViewTicketBundleDetailsForm from './ViewTicketBundleDetailsForm';
 import EngineerProfileForm from './EngineerProfile';
 import history from '../history';
@@ -17,7 +17,7 @@ class SPAEngineerMainPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showSelectTicketMsg : true
+      showSelectTicketMsg: true
     }
     this.updateRouteAndShowTicketBundleDetails = this.updateRouteAndShowTicketBundleDetails.bind(this);
     this.updateRouteAndShowTicketDetails = this.updateRouteAndShowTicketDetails.bind(this);
@@ -32,7 +32,7 @@ class SPAEngineerMainPage extends React.Component {
 
     } else if (e.target.type == 'suggestion') {
 
-    } else{
+    } else {
       history.push({
         pathname: "/ticketmanage/ticketdetails",
         search: "?ticketId=" + ticket.id
@@ -40,14 +40,14 @@ class SPAEngineerMainPage extends React.Component {
     }
   }
 
-  updateRouteAndShowTicketBundleDetails(ticketId){
+  updateRouteAndShowTicketBundleDetails(ticketId) {
     history.push({
-      pathname:'/ticketmanage/tickets',
-      search:'?ticketId='+ticketId
+      pathname: '/ticketmanage/tickets',
+      search: '?ticketId=' + ticketId
     });
-    console.log("ticketId: "+ticketId);
+    console.log("ticketId: " + ticketId);
     this.setState({
-      showSelectTicketMsg : false
+      showSelectTicketMsg: false
     });
   }
 
@@ -56,27 +56,28 @@ class SPAEngineerMainPage extends React.Component {
   }
 
   render() {
+
     return (
       <div >
         <div class='sticky' style={{ height: '100%', width: '100%' }}>
           <HeaderNavBar></HeaderNavBar>
         </div>
-        <Row style={{ background: 'rgba(0,0,0,0.3)', marginTop:'1%', marginLeft:'3%', marginRight:'3%', paddingTop:'1%' }}>
-            <Col sm='2' ><SideNavBar></SideNavBar></Col>
-            <Col sm='7' style={{border: '1px solid #E8EAED', borderRadius: '10px',  paddingRight: '5px',  paddingLeft:'5px',  paddingTop:'5px',  paddingBottom:'5px',  backgroundColor: '#ffffff'}}>
-              <Route path="/ticketmanage/dashboard" component={DashboardForm}></Route>
-              <Route path="/ticketmanage/newticket" component={CreateTicketForm}></Route>
-              <Route path="/ticketmanage/tickets" component={() => <ViewTicketsBundlesForm handleTicketBundleClick={this.updateRouteAndShowTicketBundleDetails}/>}></Route>
-              <Route path="/ticketmanage/ticketslistview" component={() => <ViewTicketsForm handleListViewTicketClick={this.updateRouteAndShowTicketDetails}></ViewTicketsForm>}></Route>
-              <Route path="/ticketmanage/ticketdetails" component={ViewTicketDetailsForm}></Route>
-            </Col>
-            <Col sm='3'>            
-              <div style={{ position: 'sticky', top: 100, zIndex: 1 }}>
-              <Route path="/ticketmanage/tickets" component={() =><ViewTicketBundleDetailsForm showSelectTicketMsg={this.state.showSelectTicketMsg}></ViewTicketBundleDetailsForm>}> </Route>
-              
-              </div>
-              <Route path="/ticketmanage/dashboard" component={() =><EngineerProfileForm ></EngineerProfileForm>}> </Route>
-            </Col>
+        <Row style={{ background: 'rgba(0,0,0,0.3)', marginTop: '1%', marginLeft: '3%', marginRight: '3%', paddingTop: '1%' }}>
+          <Col sm='2' ><SideNavBar></SideNavBar></Col>
+          <Col sm='7' style={{ border: '1px solid #E8EAED', borderRadius: '10px', paddingRight: '5px', paddingLeft: '5px', paddingTop: '5px', paddingBottom: '5px', backgroundColor: '#ffffff' }}>
+            <Route path="/ticketmanage/dashboard" component={DashboardForm}></Route>
+            <Route path="/ticketmanage/newticket" component={CreateTicketForm}></Route>
+            <Route path="/ticketmanage/tickets" component={() => <ViewTicketsBundlesForm handleTicketBundleClick={this.updateRouteAndShowTicketBundleDetails} />}></Route>
+            <Route path="/ticketmanage/ticketslistview" component={() => <ViewTicketsForm handleListViewTicketClick={this.updateRouteAndShowTicketDetails}></ViewTicketsForm>}></Route>
+            <Route path="/ticketmanage/ticketdetails" component={ViewTicketDetailsForm}></Route>
+          </Col>
+          <Col sm='3'>
+            <div style={{ position: 'sticky', top: 100, zIndex: 1 }}>
+              <Route path="/ticketmanage/tickets" component={() => <ViewTicketBundleDetailsForm showSelectTicketMsg={this.state.showSelectTicketMsg}></ViewTicketBundleDetailsForm>}> </Route>
+
+            </div>
+            <Route path="/ticketmanage/dashboard" component={() => <EngineerProfileForm ></EngineerProfileForm>}> </Route>
+          </Col>
         </Row>
       </div>
     );
@@ -85,7 +86,7 @@ class SPAEngineerMainPage extends React.Component {
 
 const mapStateToProps = function (state) {
   return {
-   // isLoadingScreenVisible: state.loadingScreen.isLoadingScreenVisible
+    // isLoadingScreenVisible: state.loadingScreen.isLoadingScreenVisible
   }
 }
 
