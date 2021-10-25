428ce250acfa73e83b9f13780eb100f29f5d40b1
Naveen Kumar Anem <naveenanem22@gmail.com>
2021-10-25 20:20:27 +0530

    test coverage

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-const Title = ({ children }) => (
-  <h1>{children}</h1>
-);
+import React from "react";
+export const simpleFunction = () => {
+  return "sample function message";
+};
+const Title = ({ children }) => <h1>{children}</h1>;
 export default Title;
