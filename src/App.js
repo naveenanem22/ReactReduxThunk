8701fe1e51f087c866938ae2d23277fe332bf58e
Naveen Kumar Anem <naveenanem22@gmail.com>
2021-10-25 17:12:49 +0530

    genericpush

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import FunctionalDemo from './components/FunctionalDemo/FunctionalDemo'
+import DropDown from './components/DropDown/DropDown';
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 //import './App.css';
 
+const items = [
+  { "id": 1, "value": "Red" },
+  { "id": 2, "value": "Blue" },
+  { "id": 3, "value": "Green" }
+
+];
 
 export default class App extends Component {
 
@@ -12,11 +19,14 @@ export default class App extends Component {
 
   render() {
 
-    return <div>
-      <h1> Hello, World!!!</h1>
-      <p>The component below is a functional component.</p>
-      <FunctionalDemo></FunctionalDemo>
-    </div>;
+    return (
+      <React.Fragment>
+        <h1> Hello, World!!!</h1>
+        <p>The component below is a functional component.</p>
+        <FunctionalDemo></FunctionalDemo>
+        <DropDown items={items} title="Custom DropDown"></DropDown>
+      </React.Fragment>
+    );
 
 
   }
